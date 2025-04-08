829d3716b298530479a836a2d37e6c1e85d429a1
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-09 02:07:50 +0530

    Added event handlers to update state for changes in input

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function SideBar() {
     firstName: '',
     lastName: '',
     email: '',
-    phoneNumber: '',
+    phone: '',
     linkedin: '',
     summary: '',
     graduation: '',
@@ -28,36 +28,46 @@ function SideBar() {
     ] 
   });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userInfo);
+  }
+
+  const handleChange = (e) => {
+    setUserInfo({ ...userInfo, [e.target.id]: e.target.value });
+    console.log(userInfo);
+  }
+
   return (
-    <div className='bg-slate-700 col-span-2 text-white'>
+    <div className='bg-slate-700 col-span-2'>
       <h1 className='text-center mt-[2.5rem] uppercase text-2xl'> Resume Generator</h1>
       {/*PERSONAL DETAILS*/}
       <div className='py-[2rem]'>
-            <h3 className='text-center text-lg'>Personal Details</h3>
+            <h3 className='text-center text-lg  text-white'>Personal Details</h3>
         <form method='POST' id='personal'>
           <section>
           <div className='flex flex-col w-4/5 m-auto gap-3'>
-          <label htmlFor='first-name'>First Name</label>
-          <input id='first-name' name='first-name' type='text' required />
+          <label htmlFor='firstName' className='text-white'>First Name</label>
+          <input id='firstName' name='firstName' type='text' onChange={handleChange}/>
 
-          <label htmlFor='last-name'>Last Name</label>
-          <input id='last-name' name='last-name' type='text' required />
+          <label htmlFor='lastName' className='text-white'>Last Name</label>
+          <input id='lastName' name='lastName' type='text' onChange={handleChange} />
 
-          <label htmlFor='email'>Email ID</label>
-          <input id='email' name='email' type='email' required />
+          <label htmlFor='email' className='text-white'>Email ID</label>
+          <input id='email' name='email' type='email' onChange={handleChange}/>
 
-          <label htmlFor='phone'>Phone</label>
-          <input id='phone' name='phone' type='text' minLength={9} required />
+          <label htmlFor='phone' className='text-white'>Phone</label>
+          <input id='phone' name='phone' type='text' minLength={9} onChange={handleChange} />
             
-          <label htmlFor='linkedin-id'>Linkedin</label>
-          <input id='linkedin-id' name='linkedin-id' type='url' pattern="https://.*" />
+          <label htmlFor='linkedin' className='text-white'>Linkedin</label>
+          <input id='linkedin' name='linkedin' type='url' pattern="https://.*" onChange={handleChange} />
               
-          <label htmlFor='summary'>Summary</label>
-          <input id='summary' name='summary' type='text'/>
+          <label htmlFor='summary' className='text-white'>Summary</label>
+          <input id='summary' name='summary' type='text' onChange={handleChange}/>
 
           </div>
           </section>
-        <Button className='flex mx-auto my-4'>Button</Button>
+        <Button type="button" className='flex mx-auto my-4' onClick={handleSubmit}>Button</Button>
         </form>
       </div>
 
@@ -66,27 +76,27 @@ function SideBar() {
         <form method='POST' className='flex flex-col justify-center items-center' id='edu'>
         <h3 className='text-center text-lg'>Education Details</h3>
       <div className='flex flex-col gap-2'>
-            <label htmlFor='graduation'>Graduation</label>
+            <label htmlFor='graduation' className='text-white'>Graduation</label>
             <div className='flex gap-2'> 
-        <input id='graduation' name='graduation' type='text' required className='w-[70%]' />
-        <input id='graduation-grade' name='graduation-grade' type='number' required className='w-[20%]' placeholder='Eg 75%'/>
+        <input id='graduation' name='graduation' type='text' required className='w-[70%]' onChange={handleChange}/>
+        <input id='grad_grade' name='grad_grade' type='number' required className='w-[20%]' placeholder='Eg 75%' onChange={handleChange}/>
             </div>
       </div>
           
       <div className='flex flex-col gap-2 my-4'>
-      <label htmlFor='hsc'>Higher Secondary/ Grade 12</label>
+      <label htmlFor='hsc' className='text-white'>Higher Secondary/ Grade 12</label>
           <div className='flex gap-2'> 
-        <input id='hsc' name='hsc' type='text' required className='w-[70%]' />
-        <input id='hsc-grade' name='hsc-grade' type='number' className='w-[20%]' placeholder='Eg 75%'/>
+        <input id='hsc' name='hsc' type='text' required className='w-[70%]' onChange={handleChange}/>
+        <input id='hsc_grade' name='hsc_grade' type='number' className='w-[20%]' placeholder='Eg 75%' onChange={handleChange}/>
            </div>
           </div>
           
 
           <div className='flex flex-col gap-2 my-4'>
-      <label htmlFor='ssc'>Senior Secondary/ Grade 10</label>
+      <label htmlFor='ssc' className='text-white'>Senior Secondary/ Grade 10</label>
           <div className='flex gap-2'> 
-        <input id='ssc' name='ssc' type='text' required className='w-[70%]' />
-        <input id='ssc-grade' name='ssc-grade' type='number' className='w-[20%]' placeholder='Eg 75%'/>
+        <input id='ssc' name='ssc' type='text' required className='w-[70%]' onChange={handleChange}/>
+        <input id='ssc_grade' name='ssc_grade' type='number' className='w-[20%]' placeholder='Eg 75%' onChange={handleChange}/>
            </div>
       </div>
     </form>
@@ -98,30 +108,30 @@ function SideBar() {
         <form method='POST' className='flex flex-col justify-center items-center' id='work-ex'>
         <h3 className='text-center text-lg'>Work Experience</h3>
       <div className='flex flex-col gap-2'>
-            <label htmlFor='title'>Title</label>
+            <label htmlFor='title' className='text-white'>Title</label>
             <div className='flex gap-2'> 
-            <input id='title' name='title' type='text' required className='w-[70%]' />
+            <input id='title' name='title' type='text' required className='w-[70%]' onChange={handleChange}/>
             </div>
       </div>
           
       <div className='flex flex-col gap-2 my-4'>
-      <label htmlFor='company'>Company Name</label>
-        <input id='company' name='company' type='text' required className='w-[70%]' />
+      <label htmlFor='company_name' className='text-white'>Company Name</label>
+        <input id='company_name' name='company_name' type='text' required className='w-[70%]' onChange={handleChange}/>
           <div className='flex gap-2'> 
            </div>
           </div>
           
 
           <div className='flex flex-col gap-2 my-4'>
-      <label htmlFor='loc-type'>Location Type</label>
-      <input id='loc-type' name='loc-type' type='text' required className='w-[70%]' />
+      <label htmlFor='loc_type' className='text-white'>Location Type</label>
+      <input id='loc_type' name='loc_type' type='text' required className='w-[70%]' onChange={handleChange}/>
 
           <div className='flex gap-2'> 
             </div>
       </div>
       
-      <label htmlFor='description'>Description</label>
-      <input id='description' name='description' type='text' required className='w-[70%]' />
+      <label htmlFor='description' className='text-white'>Description</label>
+      <input id='description' name='description' type='text' required className='w-[70%]' onChange={handleChange}/>
     </form>
       </div>
       
